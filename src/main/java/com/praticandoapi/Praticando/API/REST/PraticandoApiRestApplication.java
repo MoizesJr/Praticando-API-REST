4c1b3f8aaa74eef30abe64a7cb5b143dd713b258
Moizés Júnior <moizes.tj@gmail.com>
2023-06-13 12:45:14 -0300

    Praticando API REST

---
+++ src/main/java/com/praticandoapi/Praticando/API/REST/PraticandoApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.praticandoapi.Praticando.API.REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praticandoapi/Praticando/API/REST/PraticandoApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticandoapi.Praticando.API.REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
